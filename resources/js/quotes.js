e32c0c1a0ec3d92cb48eb0f6f18debe94a885bbd
Terence <trcexcel@yahoo.com>
2017-02-11 20:35:48 -0600

    Removed some of the Welcome Greetings.

--- resources/js/quotes.js
+++ resources/js/quotes.js
@@ -15,9 +15,8 @@ var welcomeAffirmationWords = ['loved', 'cherished', 'awesome', 'amazing', 'a st
     'sensational', 'serene', 'spectacular', 'strong', 'terrific', 'trusting', 'unlimited', 'limitless', 'uplifted',
     'valuable', 'vibrant', 'vivacious', 'warm', 'welcomed', 'whole', 'wise', 'worthy', 'zestful'];
 var welcomeGreetingWords = ['counted your blessings', 'smiled', 'loved someone', 'loved yourself', 'breathed deeply',
-    'read His Word', 'meditated', 'blessed someone', 'encouraged someone', 'called a friend', 'hugged someone',
-    'enjoyed your life', 'given life your best', 'eaten well', 'exercised', 'treated yourself well', 'loved your life',
-    'told someone you love them', 'danced in victory', 'walked in peace'];
+    'read His Word', 'meditated', 'blessed someone', 'encouraged someone', 'hugged someone',
+    'enjoyed your life', 'treated yourself well', 'walked in peace'];
 var displayQuote = false;
 var displayImage = false;
 
